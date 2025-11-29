da1fdc6a1475ee27361bbeec96dae3a057e8db31
SGT_KROXX! <110301304+GhostGamerz67638@users.noreply.github.com>
2025-11-29 14:42:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,11 @@ dotenv.load_dotenv()
 
 
 class BaseConfig(object):
-    BOT_TOKEN = getenv("BOT_TOKEN")
+    BOT_TOKEN = getenv("BOT_TOKEN", "8267726453:AAGaMshUPP1AEkJUUFeI4u1uFsB7WjLEzco")
     OWNER_ID = int(getenv("OWNER_ID", 1207066133))
-    API_ID = int(getenv("API_ID"))
-    API_HASH = getenv("API_HASH")
+    API_ID = int(getenv("API_ID", "32461689"))
+    API_HASH = getenv("API_HASH", "1049552a00c9134e1c93e6580289261e
+")
     LOG_CHAT_ID = int(getenv("LOG_CHAT_ID", "-1001482059289"))
     LOG_LEVEL = getenv("LOG_LEVEL") or "INFO"
     CHNL_NAME = getenv("UPDATES_CHANNEL", "bots_universe")
